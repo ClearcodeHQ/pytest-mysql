55ccecdb7b85eb145e5816708c816f5fd26e14da
Grzegorz Śliwiński <g.sliwinski@clearcode.cc>
2018-02-09 15:47:12 +0100

    fixup: pylama

--- tests/test_executor.py
+++ tests/test_executor.py
@@ -1,4 +1,4 @@
-from io import BytesIO
+"""Executor tests."""
 
 import pytest
 from mock import patch
@@ -8,10 +8,17 @@ from pytest_mysql.executor import MySQLExecutor, MySQLUnsupported
 
 @pytest.mark.parametrize('verstr, version', (
     (b'mysql_install_db Ver 5.7.21, for Linux on x86_64', '5.7.21'),
-    (b'mysqld  Ver 5.7.21-0ubuntu0.17.10.1 for Linux on x86_64 ((Ubuntu))', '5.7.21'),
+    (
+        (
+            b'mysqld  Ver 5.7.21-0ubuntu0.17.10.1 '
+            b'for Linux on x86_64 ((Ubuntu))'
+        ),
+        '5.7.21'
+    ),
     (b'mysql 5.5.55', '5.5.55')
 ))
 def test_version_check(verstr, version, tmpdir_factory):
+    """Test executor's version property."""
     executor = MySQLExecutor(
         mysqld_safe='',
         mysqld='',
@@ -37,6 +44,7 @@ def test_version_check(verstr, version, tmpdir_factory):
     b'mysql 5.5.55',
 ))
 def test_exception_raised(verstr, tmpdir_factory):
+    """Raise exception on not supported versions."""
     executor = MySQLExecutor(
         mysqld_safe='',
         mysqld='',
